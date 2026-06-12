a67476afae4249e22378e9a9107209d96ced9440
Fillip <103233073+ByungilOh-Fillip@users.noreply.github.com>
2026-06-12 16:25:04 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { fileURLToPath, URL } from "node:url";
 const projectRoot = fileURLToPath(new URL(".", import.meta.url));
 
 export default defineConfig({
+  base: "/Material_Helper_Byungil/",
   root: projectRoot,
   plugins: [react()],
   build: {
